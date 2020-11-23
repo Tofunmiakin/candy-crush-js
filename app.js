0351f82d545ee8f6d77aac022fcd47d6d342ad2a
R04DM4N <58348842+Tofunmiakin@users.noreply.github.com>
2020-11-23 06:03:55 +0100

    movement

--- app.js
+++ app.js
@@ -18,12 +18,71 @@ document.addEventListener('DOMContentLoaded', () => {
   const createBoard = () =>{
     for (let i = 0; i < width*width; i++) {
       const square = document.createElement('div')
+      
+      square.setAttribute('draggable', true)
+      square.setAttribute('id', i)
+
+
       let randomColor = Math.floor(Math.random() * candyColors.length)
       square.style.backgroundColor = candyColors[randomColor]
+
       grid.appendChild(square)
       squares.push(square)
     }
   }
 
   createBoard()
+
+
+
+  //Dragging the candies
+  let colorBeingDragged
+  let colorbeingReplaced
+  let squareIdBeingDragged
+  let squareIdBeingReplaced
+
+  squares.forEach(square => square.addEventListener('dragstart', dragStart) )
+  squares.forEach(square => square.addEventListener('dragend', dragEnd) )
+  squares.forEach(square => square.addEventListener('dragover', dragOver) )
+  squares.forEach(square => square.addEventListener('dragenter', dragEnter) )
+  squares.forEach(square => square.addEventListener('dragleave', dragLeave) )
+  squares.forEach(square => square.addEventListener('drop', dragDrop) )
+
+  function dragStart(){
+    colorBeingDragged = this.style.backgroundColor
+    squareIdBeingDragged = parseInt(this.id)
+    console.log(this.id, 'dragstart')
+  }
+
+  function dragEnd (){
+    console.log(this.id, 'dragend')
+  }
+
+  function dragOver(e){
+    e.preventDefault()
+    console.log(this.id, 'dragover')
+  }
+
+  function dragEnter(e){
+    e.preventDefault()
+    console.log(this.id, 'dragenter')
+  }
+
+  function dragLeave (){
+    console.log(this.id, 'dragleave')
+  }
+
+  function dragDrop(){
+    colorbeingReplaced = this.style.backgroundColor
+    squareIdBeingReplaced = parseInt(this.id)
+    this.style.backgroundColor = colorBeingDragged
+    squares[squareIdBeingDragged].style.backgroundColor = colorbeingReplaced
+    console.log(this.id, 'drop')
+  }
+
+
 }) 
+
+
+
+  
